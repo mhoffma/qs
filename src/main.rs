c4c8f781a58a2f6d8031d948291baac13d6038f1
marc <marc.m.hoffman@gmail.com>
2023-02-04 06:48:05 -0500

    slices

--- src/main.rs
+++ src/main.rs
@@ -93,6 +93,53 @@ fn quick_sort<T: SortTraits>(x: &Vec<T>) -> Vec<T> {
     y
 }
 
+#[allow(dead_code)]
+fn quick_sort_slice<T: SortTraits>(x: &mut [T]) -> &[T] {
+    // Sorts a (portion of an) array, divides it into partitions, then sorts those
+    fn quicksort<T: SortTraits>(a: &mut [T], lo: i32, hi: i32) {
+        // Ensure indices are in correct order
+        if lo >= hi || lo < 0 {
+            return;
+        }
+
+        // Partition array and get the pivot index
+        let l = usize::try_from(lo).unwrap();
+        let h = usize::try_from(hi).unwrap();
+        let p = partition(a, l, h);
+
+        // Sort the two partitions
+        quicksort(a, lo, p - 1); // Left side of pivot
+        quicksort(a, p + 1, hi); // Right side of pivot
+    }
+    // Divides array into two partitions
+    fn partition<T: SortTraits>(y: &mut [T], lo: usize, hi: usize) -> i32 {
+        let pivot = y[hi].clone(); // Choose the last element as the pivot
+                                   // Temporary pivot index
+        let mut i = lo;
+
+        for j in lo..hi {
+            // If the current element is less than or equal to the pivot
+            if y[j] <= pivot {
+                // Move the temporary pivot index forward
+                // Swap the current element with the element at the temporary pivot index
+                let t = y[i].clone();
+                y[i] = y[j].clone();
+                y[j] = t;
+                i = i + 1;
+            }
+        }
+        // Move the pivot element to the correct pivot position (between the smaller and larger elements)
+        let t = y[i].clone();
+        y[i] = y[hi].clone();
+        y[hi] = t;
+        i = i + 1;
+        i32::try_from(i).unwrap() - 1 // the pivot index
+    }
+
+    quicksort(x, 0, i32::try_from(x.len()).unwrap() - 1);
+    x
+}
+
 fn main() {
     let mut rng = rand::thread_rng();
 
@@ -103,4 +150,5 @@ fn main() {
     println!("S={:?}", quick_sort(&i));
     let mut x = i.clone();
     println!("R={:?}", bubble_sort_slice(&mut x[0..20]));
+    println!("Q={:?}", quick_sort_slice(&mut i.clone()));
 }
